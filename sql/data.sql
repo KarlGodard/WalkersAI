59a1fcd1f55af3339cd96eab2fdd76407985c943
jbrill10 <jeff.brill01@gmail.com>
2022-10-13 14:32:57 -0400

    Set up basic database for MOMA data

--- sql/data.sql
+++ sql/data.sql
@@ -1,94 +1,31 @@
 PRAGMA foreign_keys = ON;
 
-INSERT INTO users(username, fullname, email, filename, password)
-VALUES('awdeorio', 'Andrew DeOrio','awdeorio@umich.edu', 'e1a7c5c32973862ee15173b0259e3efdb6a391af.jpg',
-'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8');
+INSERT INTO artworks(title,
+  constituentID,
+  date,
+  medium,
+  dimensions,
+  creditLine,
+  accessionNumber,
+  classification,
+  department,
+  dateAcquired,
+  cataloged,
+  objectID,
+  url,
+  ThumbnailURL,
+  circumference,
+  depth,
+  diameter,
+  height,
+  length,
+  weight,
+  width,
+  seatHeight,
+  duration)
+VALUES("Ferdinandsbrücke Project, Vienna, Austria (Elevation, preliminary version)","Otto Wagner",6210,"(Austrian, 1841–1918)","(Austrian)","(1841)",1918,"Male",1896,"Ink and cut-and-pasted painted pages on paper","19 1/8 x 66 1/2 (48.6 x 168.9 cm)","Fractional and promised gift of Jo Carole and Ronald S. Lauder",885.1996,"Architecture",'Architecture & Design',1996-04-09,'Y',2,'http://www.moma.org/collection/works/2','http://www.moma.org/media/W1siZiIsIjU5NDA1Il0sWyJwIiwiY29udmVydCIsIi1yZXNpemUgMzAweDMwMFx1MDAzZSJdXQ.jpg?sha=137b8455b1ec6167',,,,48.6,,,168.9,,
+)
 
-INSERT INTO users(username, fullname, email, filename, password)
-VALUES('jflinn', 'Jason Flinn','jflinn@umich.edu', '505083b8b56c97429a728b68f31b0b2a089e5113.jpg',
-'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8');
-
-INSERT INTO users(username, fullname, email, filename, password)
-VALUES('michjc', 'Michael Cafarella','michjc@umich.edu', '5ecde7677b83304132cb2871516ea50032ff7a4f.jpg',
-'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8');
-
-INSERT INTO users(username, fullname, email, filename, password)
-VALUES('jag', 'H.V. Jagadish', 'jag@umich.edu', '73ab33bd357c3fd42292487b825880958c595655.jpg',
-'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8');
-
-INSERT INTO posts(filename, owner)
-VALUES('122a7d27ca1d7420a1072f695d9290fad4501a41.jpg', 'awdeorio');
-
-INSERT INTO posts(filename, owner)
-VALUES('ad7790405c539894d25ab8dcf0b79eed3341e109.jpg', 'jflinn');
-
-INSERT INTO posts(filename, owner)
-VALUES('9887e06812ef434d291e4936417d125cd594b38a.jpg', 'awdeorio');
-
-INSERT INTO posts(filename, owner)
-VALUES('2ec7cf8ae158b3b1f40065abfb33e81143707842.jpg', 'jag');
-
-INSERT INTO following(username1,username2)
-VALUES ('awdeorio','jflinn');
-
-INSERT INTO following(username1,username2)
-VALUES ('awdeorio','michjc');
-
-INSERT INTO following(username1,username2)
-VALUES ('jflinn','awdeorio');
-
-INSERT INTO following(username1,username2)
-VALUES ('jflinn','michjc');
-
-INSERT INTO following(username1,username2)
-VALUES ('michjc','awdeorio');
-
-INSERT INTO following(username1,username2)
-VALUES ('michjc','jag');
-
-INSERT INTO following(username1,username2)
-VALUES ('jag','michjc');
-
-INSERT INTO comments(owner, postid, text)
-VALUES ('awdeorio',3, '#chickensofinstagram');
-
-INSERT INTO comments(owner, postid, text)
-VALUES ('jflinn', 3, 'I <3 chickens');
-
-INSERT INTO comments(owner, postid,text)
-VALUES ('michjc',3, 'Cute overload!');
-
-INSERT INTO comments(owner, postid,text)
-VALUES ('awdeorio',2, 'Sick #crossword');
-
-INSERT INTO comments(owner, postid,text)
-VALUES ('jflinn',1, 'Walking the plank #chickensofinstagram');
-
-INSERT INTO comments(owner, postid,text)
-VALUES ('awdeorio',1, 'This was after trying to teach them to do a #crossword');
-
-INSERT INTO comments(owner, postid,text)
-VALUES ('jag',4, 'Saw this on the diag yesterday!');
-
-INSERT INTO likes(owner, postid)
-VALUES('awdeorio', 1);
-
-INSERT INTO likes(owner, postid)
-VALUES('michjc', 1);
-
-INSERT INTO likes(owner, postid)
-VALUES('jflinn', 1);
-
-INSERT INTO likes(owner, postid)
-VALUES('awdeorio', 2);
-
-INSERT INTO likes(owner, postid)
-VALUES('michjc', 2);
-
-INSERT INTO likes(owner, postid)
-VALUES('awdeorio', 3);
-
-
-
-
-
+-- INSERT INTO users(username, fullname, email, filename, password)
+-- VALUES('awdeorio', 'Andrew DeOrio','awdeorio@umich.edu', 'e1a7c5c32973862ee15173b0259e3efdb6a391af.jpg',
+-- 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8');
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,35 +1,73 @@
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE users(
-  username VARCHAR(20) NOT NULL PRIMARY KEY,
-  fullname VARCHAR(40) NOT NULL,
-  email VARCHAR(40) NOT NULL,
-  filename VARCHAR(64) NOT NULL,
-  password VARCHAR(256) NOT NULL,
-  created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+CREATE TABLE artworks(
+  title VARCHAR(60) NOT NULL,
+  constituentID INTEGER NOT NULL,
+  date VARCHAR(20),
+  medium VARCHAR(40),
+  dimensions VARCHAR(40),
+  creditLine VARCHAR(60),
+  accessionNumber INTEGER,
+  classification VARCHAR(30),
+  department VARCHAR(40),
+  dateAcquired TIMESTAMP,
+  cataloged VARCHAR(2),
+  objectID INTEGER NOT NULL PRIMARY KEY,
+  url VARCHAR(60),
+  ThumbnailURL VARCHAR(60),
+  circumference INTEGER,
+  depth INTEGER,
+  diameter INTEGER,
+  height INTEGER,
+  length INTEGER,
+  weight INTEGER,
+  width INTEGER,
+  seatHeight INTEGER,
+  duration INTEGER,
+  Foreign Key(constituentID) References Artists(constituentID)
 );
-CREATE TABLE posts(
-  postid INTEGER PRIMARY KEY AUTOINCREMENT,
-  filename VARCHAR(64) NOT NULL,
-  owner VARCHAR(20) NOT NULL REFERENCES users ON DELETE CASCADE,
-  created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+-- Title,
+-- ConstituentID, - ID number of the artist in the artists table
+-- Date, -- This will be a string for now as some dates are written: 1977-1978
+-- Medium,
+-- Dimensions,
+-- CreditLine,
+-- AccessionNumber,
+-- Classification,
+-- Department,
+-- DateAcquired,
+-- Cataloged,
+-- ObjectID, - Actual ID/row number of the artwork
+-- URL,
+-- ThumbnailURL,
+-- Circumference (cm),
+-- Depth (cm),
+-- Diameter (cm),
+-- Height (cm),
+-- Length (cm),
+-- Weight (kg),
+-- Width (cm),
+-- Seat Height (cm),
+-- Duration (sec.)
+
+CREATE TABLE artists(
+  constituentID INTEGER NOT NULL PRIMARY KEY,
+  displayName VARCHAR(30),
+  artistBio VARCHAR(40),
+  nationality VARCHAR(30),
+  gender VARCHAR(10),
+  beginDate INTEGER,
+  endDate INTEGER,
+  wikiQID VARCHAR(10),
+  ULAN INTEGER
 );
-CREATE TABLE following(
-  username1 VARCHAR(20) REFERENCES users ON DELETE CASCADE,
-  username2 VARCHAR(20) REFERENCES users ON DELETE CASCADE,
-  created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  PRIMARY KEY(username1, username2)
-);
-CREATE TABLE comments(
-  commentid INTEGER PRIMARY KEY AUTOINCREMENT,
-  owner VARCHAR(20) REFERENCES users ON DELETE CASCADE,
-  postid INTEGER REFERENCES posts ON DELETE CASCADE,
-  text VARCHAR(1024),
-  created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-CREATE TABLE likes(
-  likeid INTEGER PRIMARY KEY AUTOINCREMENT,
-  owner VARCHAR(20) REFERENCES users ON DELETE CASCADE,
-  postid INTEGER REFERENCES posts ON DELETE CASCADE,
-  created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
-)
+
+-- ConstituentID,
+-- DisplayName,
+-- ArtistBio, - This is just their nationality and year of birth/sometimes death
+-- Nationality,
+-- Gender,
+-- BeginDate, - Unsure, but it looks like a 0 here means undefined/uncertain
+-- EndDate, - Same as above
+-- Wiki QID,
+-- ULAN - Getty ID

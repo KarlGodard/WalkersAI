282a47a637bc98a8c282f474e42e04a09a2a5181
lilian-emchu <lilian.emchu@gmail.com>
2022-12-09 18:36:36 -0500

    Fixed trivia update function

--- sketchy/views/__init__.py
+++ sketchy/views/__init__.py
@@ -2,4 +2,4 @@
 from sketchy.views.login import login, logout, create_account, logged_in
 from sketchy.views.index import show_index
 from sketchy.views.favorites import favorites, chat
-from sketchy.views.user import handle_follow_user, show_user, upload_img
+from sketchy.views.trivia import trivia_score
